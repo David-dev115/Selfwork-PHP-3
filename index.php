28dc9416f8d24a0a3e2ddc09e871d2283d142a09
David Paparo <d.paparo@tiscali.it>
2025-07-21 15:17:33 +0200

    aggiunta correzione

--- index.php
+++ index.php
@@ -81,22 +81,42 @@ $words2 = [
 ];
 
 
-$parola_1 = "Nel";
+
+$words1 [] = "Nel";
+
+$words1 [] = "di cammin di nostra vita";
+
+$words1 [] = "una selva";
+
+$words1 [] = ", che la diritta via";
+
+$words1 [] = "smarrita";
+
+
+
+
+
+$parola_1 = $words1[8];
 $parola_2 = $words1[6][3][0];
-$parola_3 = "di cammin di nostra vita";
+$parola_3 = $words1[9];
 $parola_4 = $words1[4];
 $parola_5 = $words1[6][4];
 $parola_6 = $words1[6][5];
-$parola_7 = "una selva";
+$parola_7 = $words1[10];
 $parola_8 = $words1[6][0];
-$parola_9 = ", che la diritta via";
+$parola_9 = $words1[11];
 $parola_10 = $words1[6][1];
-$parola_11 = "smarrita";
+$parola_11 = $words1[12];
 $space = " ";
+
+//carico esercizio completo
+
+
+
+print_r($words1);
 
 $results = $parola_1 . $space  . $parola_2 . $space . $parola_3 . $space . $parola_4 . $space . $parola_5 . $space . $parola_6 . $space . $parola_7 . $space . $parola_8 . $parola_9 . $space . $parola_10 . $space . $parola_11 . "." . "\n";
 
 echo $results;
 
-
-//carico esercizio completo
+// correzione 
